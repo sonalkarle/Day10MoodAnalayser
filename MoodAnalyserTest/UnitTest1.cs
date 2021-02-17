d568569bba41765f5d2578464d6af4834ec13d2b
sonal <sonalkarle01@gmail.com>
2021-02-17 17:57:58 +0530

    Check mood by the respond of message

--- MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyserTest/UnitTest1.cs
@@ -1,18 +1,38 @@
 using NUnit.Framework;
-
+using MoodAnalyserNameSpace;
 namespace MoodAnalyserTest
 {
     public class Tests
     {
+        MoodAnalyserClass moodAnalyser;
         [SetUp]
         public void Setup()
         {
+            moodAnalyser = new MoodAnalyserClass();
         }
 
         [Test]
-        public void Test1()
+        // TC 1.1: Given message "I am in SAD Mood " return the "SAD"
+        public void Given_Message_Should_Return_Sad()
         {
-            Assert.Pass();
+            string message = "I am in SAD mood";
+            string expected = "SAD";
+            string actual = moodAnalyser.MoodAnalyserMethod(message);
+            Assert.AreEqual(expected, actual);
         }
+
+        [Test]
+        //TC 1.2: Given message "I am in HAPPY Mood " return the "HAPPY"
+
+        public  void Given_Message_Should_Return_Happy()
+        {
+            string message = "I am in HAPPY mood";
+            string expected = "HAPPY";
+            string actual = moodAnalyser.MoodAnalyserMethod(message);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        
     }
 }
