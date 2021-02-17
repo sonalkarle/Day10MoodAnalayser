849f01bc9e2c08c906ae27884a3cce43b7f0767f
sonal <sonalkarle01@gmail.com>
2021-02-17 17:42:22 +0530

    Welcome

--- MoodAnalyser/Program.cs
+++ MoodAnalyser/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 
-namespace MoodAnalyser
+namespace MoodAnalyserNameSpace
 {
     class Program
-    {
+    { 
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Mood analyser");
         }
     }
 }
